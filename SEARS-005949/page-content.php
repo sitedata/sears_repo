fb9e828a1a0014ca4ba0e759c197cae169eb1ec1
Bob Ware <bob.ware@purered.net>
2018-07-20 13:36:51 -0400

    Finish Laundry Hacks

--- SEARS-005949/page-content.php
+++ SEARS-005949/page-content.php
@@ -16,16 +16,12 @@ HERO MUST BE OUTSIDE OF DIV.CONTAINER TO BE FULL WIDTH!
   </section>
   <!-- END #hero -->
 
-<!-- extra wrapper around all these things that are spozta be 65px narrower than hero -->
-<div class="padding-horiz-xl">
-
-  <!-- HEADLINE -->
-  <section class="container headline">
+  <section class="container headline padding-vert-xl">
     <div class="col-xs-12">
       <div class="row">
         <div class="col-xs-12">
           <div class="headline--copy text-align-center font--primary2 font--600">
-            <?php echo clean_text( $headline ) ?>
+            <?php echo htmlspecialchars( $headline ) ?>
           </div>
         </div>
         <div class="col-xs-12 col-sm-12 fake-bottom-border">
@@ -35,7 +31,7 @@ HERO MUST BE OUTSIDE OF DIV.CONTAINER TO BE FULL WIDTH!
     </div>
   </section>
 
-  <section class="container laundry-essentials">
+  <section class="container laundry-hacks">
 
     <div class="col-xs-12">
 <?php foreach( $content as $i => $row ): ?>
@@ -43,60 +39,34 @@ HERO MUST BE OUTSIDE OF DIV.CONTAINER TO BE FULL WIDTH!
 // lazy
 $img = $img_dir . $row['img'];
 $heading = clean_text( $row['heading'] );
-$copy = clean_text( $row['copy'] );
-
+$copy = $row['copy']; // already cleaned it and it may have a link it!
+$extra = isset( $row['extra'] ) ? clean_text( $row['extra'] ) : false;
+$extra_img = isset( $row['extra_img'] ) ? $img_dir . $row['extra_img'] : false;
 ?>
-<?php if ( $i % 2 == 0 ): ?>
-            <div class="row">
+      <div class="row laundry-hack laundry-hack--<?php printf( "%02d", $i + 1 ) ?>">
+        <div class="col-sm-12 col-md-6 col-left">
+            <img src="<?php echo $img ?>" class="width100percent" alt="<?php echo $heading ?>">
+        </div>
+        <div class="col-sm-12 col-md-6 padding-horiz-xl col-right">
+          <h3 class="font--400 font--primary2"><?php echo $heading ?></h3>
+          <p class="font--black"><?php echo $copy ?></p>
+<?php if ( false !== $extra && !empty( $extra ) ): ?>
+          <a href="#" class="sticker--link"><?php if ($extra_img): ?><img src="<?php echo $extra_img; ?>" class="sticker--icon"><?php endif; ?> <span class="sticker--link_text"><?php echo $extra ?></span></a>
 <?php endif; ?>
-              <div class="col-sm-12 col-md-6">
-                  <img src="<?php echo $img ?>" class="width100percent" alt="<?php echo $heading ?>">
-                  <div class="padding-vert-xl">
-                    <h3 class="font--400 font--primary2"><?php echo $heading ?></h3>
-                    <p class="font--black"><?php echo $copy ?></p>
-                  </div>
-              </div>
-<?php if ( $i % 2 == 1 ): ?>
-            </div><!-- div.row -->
-<?php endif; ?>
+        </div>
+      <?php if ( true || $row != end( $content ) ): ?>
+        <div class="col-xs-12 col-sm-12 fake-bottom-border">
+          <hr class="bordered--bottom bordered--sm">
+        </div>
+      <?php endif; ?>
+    </div><!-- div.row.laundry-hack -->
 <?php endforeach; ?>
     </div><!-- div.col-xs-12 -->
-  </section>
-
-        <!-- FOOTER-ISH HEADLINE -->
-        <section class="container headline footer-ish--headline">
-<?php
-/**
-MOTHERSCRATCHING, INCONSISTENT, FAKE BORDERS THAT WANT TO BUST OUT OF BOXES!
-*/
-?>
-          <div class="fake-bottom-border">
-            <hr class="visible-xs visible-sm bordered--bottom bordered--sm">
-          </div>
-
-          <div class="col-xs-12">
-            <div class="row">
-              <div class="col-xs-12">
-                <div class="headline--copy text-align-center font--primary2 font--600">
-                  <?php echo clean_text( $footer_copy ) ?>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+  </section><!-- section.laundry-hacks -->
 
 <?php
-
-/**
-Add a MFn additional parameter to allow for hiding the brand_logos_heading
-to match the inconsistent design!
-*/
-
 // use element to include reusable page-element snippet!
-element( 'brand-logos.php',
-  compact( 'brand_logos_heading', 'brand_logos_heading_wrapper_classes' )
-);
+element( 'brand-logos.php' );
 ?>
-  </div><!-- div.pading-horiz-xl -->
+
 </div><!-- #cms-content-1920-1920.cms-content.cms-content-html -->
