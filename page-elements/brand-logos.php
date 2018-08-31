fa16154cb0938e6860a58a7fbd409e9ab6c340c0
Bob Ware <bob.ware@purered.net>
2018-08-31 18:18:23 -0400

    Update common elements

--- page-elements/brand-logos.php
+++ page-elements/brand-logos.php
@@ -1,40 +1,58 @@
 <?php
 
+$brand_logos = isset( $brand_logos ) ? $brand_logos : array();
+$brand_logos = is_array( $brand_logos ) ? $brand_logos : compact( 'brand_logos' );
+
+$_defaults = array(
+  'headline' => 'Ready to get started?',
+  'copy' => 'Sears Hometown Store has unbeatable prices on the best brands available.',
+  'link_url' => '#',
+  'classes' => '',
+  'btn_url' => '#',
+  'logo_dir' => '/assets/cms/brand-logos/',
+);
+
+$brand_logos = array_merge( $_defaults, $brand_logos );
+
 if ( defined( 'SEARS_USE_SANDBOX_ASSETS' ) && SEARS_USE_SANDBOX_ASSETS ) {
-  $logo_dir = 'http://hometown.sb3.production.netsuitestaging.com/assets/cms/purered/brand-logos/logos/';
+  $brand_logos['logo_dir'] = 'http://hometown.sb3.production.netsuitestaging.com/assets/cms/purered/brand-logos/logos/';
 }
-else if ( defined( 'SEARS_PROJECT_PATH' ) ) {
-  $logo_dir = get_relative_path( SEARS_PROJECT_PATH . '/img/logos/' );
+// else if ( defined( 'SEARS_PROJECT_PATH' ) ) {
+//   $logo_dir = get_relative_path( SEARS_PROJECT_PATH . '/img/logos/' );
+// }
+// else {
+//   $logo_dir = '/assets/cms/brand-logos/';
+// }
+
+
+if ( false !== strpos( $brand_logos['copy'], '%1$s' ) ) {
+  $brand_logos['copy'] = sprintf( $brand_logos['copy'], $brand_logos['link_url'] );
 }
-else {
-  $logo_dir = '/assets/cms/brand-logos/';
-}
-
-$brand_logos_link_url = isset( $brand_logos_link_url ) ? $brand_logos_link_url : '#';
-
-$brand_logos_heading = isset( $brand_logos_heading ) ?
-  $brand_logos_heading :
-  'Shop the latest <a href="%1$s" data-href="%1$s"class="font--primary2 font--700">appliances</a> for the best in kitchen solutions. We have unbeatable prices on the best brands available.';
-
-if ( false !== strpos( $brand_logos_heading, '%1$s' ) ) {
-  $brand_logos_heading = sprintf( $brand_logos_heading, $brand_logos_link_url );
-}
-
-$brand_logos_heading_wrapper_classes = isset( $brand_logos_heading_wrapper_classes ) ?
-  ' ' . $brand_logos_heading_wrapper_classes : '';
-
-$shop_now_url = isset( $shop_now_url ) ? $shop_now_url : '#';
 
 $logos = isset( $logos ) ? $logos :
   array(
-    'Kenmore' => '01_Kenmore.png',
-  	'Whirlpool' => '02_Whirlpool.png',
-    'GE' => '03_GE.png',
-  	'Frigidaire' => '04_Frigidaire.png',
-  	'LG' => '05_LG.png',
-  	'Samsung' => '06_Samsung.png',
-  	'Bosch' => '07_Bosch.png',
-  	'KitchenAid' => '08_KitchenAid.png',
+    'Kenmore' => 'kenmore.svg',
+    'Kenmore Elite' => 'kenmore-elite.svg',
+  	'Whirlpool' => 'whirlpool.svg',
+  	'Frigidaire' => 'frigidaire.svg',
+  	'Samsung' => 'samsung.svg',
+    'Maytag' => 'maytag.png',
+    'LG' => 'lg.svg',
+    'KitchenAid' => 'kitchenAid.svg',
+    'GE' => 'ge.svg',
+  	'Bosch' => 'bosch.svg',
+
+// PNGs
+    // 'Kenmore' => 'kenmore.png',
+    // 'Kenmore Elite' => 'kenmore-elite.png',
+  	// 'Whirlpool' => 'whirlpool.png',
+  	// 'Frigidaire' => 'frigidaire.png',
+  	// 'Samsung' => 'samsung.png',
+    // 'Maytag' => 'maytag.png',
+    // 'LG' => 'lg.png',
+    // 'KitchenAid' => 'kitchenAid.png',
+    // 'GE' => 'ge.png',
+  	// 'Bosch' => 'bosch.png',
   );
 
 ?>
@@ -42,9 +60,12 @@ $logos = isset( $logos ) ? $logos :
         <section class="container brand-logos">
 
           <!-- BRAND LOGOS HEADING -->
-          <div class="row<?php echo $brand_logos_heading_wrapper_classes ?>">
-            <div class="col-xs-12 padding-vert-xl">
-              <strong class="brand-logos--heading font--black font-18 lh-md"><?php echo $brand_logos_heading ?></strong>
+          <div class="row<?php echo $brand_logos['classes'] ?>">
+            <div class="col-xs-12 padding-vert-xl text-align-center">
+            <?php if ( !empty( $brand_logos['headline'] ) ): ?>
+              <h2 class="brand-logos__headline font-40 font--700 lh-sm"><?php echo $brand_logos['headline'] ?></h2>
+            <?php endif; ?>
+              <p class="brand-logos__copy font--black font-31 lh-md"><?php echo $brand_logos['copy'] ?></p>
             </div>
           </div>
 
@@ -59,7 +80,7 @@ $i = 0;
               <div class="brand-logos--wrapper">
   <?php endif; ?>
                 <div class="brand-logo">
-                  <img src="<?php echo $logo_dir . $logo ?>" alt="<?php echo $brand ?>" class="">
+                  <img src="<?php echo $brand_logos['logo_dir'] . $logo ?>" alt="<?php echo $brand ?>" class="">
                 </div>
   <?php if ( $logo === end( $logos ) || $i == (( count( $logos ) / 2 ) - 1) ): ?>
               </div>
@@ -70,7 +91,9 @@ $i = 0;
           </div>
           <div class="row">
             <div class="col-xs-12 padding-vert-xl text-align-center">
-              <a href="<?php echo $shop_now_url ?>" data-href="<?php echo $shop_now_url ?>" class="btn btn-lg btn--shop-now"><span>shop</span>now</a>
+              <a href="<?php echo $brand_logos['btn_url'] ?>"
+                data-href="<?php echo $brand_logos['btn_url'] ?>"
+                class="btn btn-lg btn--shop-now"><span>shop</span>now</a>
             </div>
           </div>
         </section><!-- .col-xs-12 -->
---
+++ page-elements/pre-20180905_brand-logos.php
@@ -0,0 +1,76 @@
+<?php
+
+if ( defined( 'SEARS_USE_SANDBOX_ASSETS' ) && SEARS_USE_SANDBOX_ASSETS ) {
+  $logo_dir = 'http://hometown.sb3.production.netsuitestaging.com/assets/cms/purered/brand-logos/logos/';
+}
+else if ( defined( 'SEARS_PROJECT_PATH' ) ) {
+  $logo_dir = get_relative_path( SEARS_PROJECT_PATH . '/img/logos/' );
+}
+else {
+  $logo_dir = '/assets/cms/brand-logos/';
+}
+
+$brand_logos_link_url = isset( $brand_logos_link_url ) ? $brand_logos_link_url : '#';
+
+$brand_logos_heading = isset( $brand_logos_heading ) ?
+  $brand_logos_heading :
+  'Shop the latest <a href="%1$s" data-href="%1$s"class="font--primary2 font--700">appliances</a> for the best in kitchen solutions. We have unbeatable prices on the best brands available.';
+
+if ( false !== strpos( $brand_logos_heading, '%1$s' ) ) {
+  $brand_logos_heading = sprintf( $brand_logos_heading, $brand_logos_link_url );
+}
+
+$brand_logos_heading_wrapper_classes = isset( $brand_logos_heading_wrapper_classes ) ?
+  ' ' . $brand_logos_heading_wrapper_classes : '';
+
+$shop_now_url = isset( $shop_now_url ) ? $shop_now_url : '#';
+
+$logos = isset( $logos ) ? $logos :
+  array(
+    'Kenmore' => '01_Kenmore.png',
+  	'Whirlpool' => '02_Whirlpool.png',
+    'GE' => '03_GE.png',
+  	'Frigidaire' => '04_Frigidaire.png',
+  	'LG' => '05_LG.png',
+  	'Samsung' => '06_Samsung.png',
+  	'Bosch' => '07_Bosch.png',
+  	'KitchenAid' => '08_KitchenAid.png',
+  );
+
+?>
+<!-- BRAND LOGOS -->
+        <section class="container brand-logos">
+
+          <!-- BRAND LOGOS HEADING -->
+          <div class="row<?php echo $brand_logos_heading_wrapper_classes ?>">
+            <div class="col-xs-12 padding-vert-xl">
+              <strong class="brand-logos--heading font--black font-18 lh-md"><?php echo $brand_logos_heading ?></strong>
+            </div>
+          </div>
+
+          <div class="row">
+<?php
+// place to store index since $logos is an associative array
+$i = 0;
+?>
+<?php foreach ( $logos as $brand => $logo ): ?>
+  <?php if ( $i == 0 || $i == ceil(count( $logos ) / 2) ): ?>
+            <div class="col-xs-12 col-sm-12 col-md-6 brand-logos--col<?php if ( $i !== 0 ): echo ' brand-logos--col_right'; endif; ?>">
+              <div class="brand-logos--wrapper">
+  <?php endif; ?>
+                <div class="brand-logo">
+                  <img src="<?php echo $logo_dir . $logo ?>" alt="<?php echo $brand ?>" class="">
+                </div>
+  <?php if ( $logo === end( $logos ) || $i == (( count( $logos ) / 2 ) - 1) ): ?>
+              </div>
+            </div>
+  <?php endif; ?>
+  <?php $i++; ?>
+<?php endforeach; ?>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 padding-vert-xl text-align-center">
+              <a href="<?php echo $shop_now_url ?>" data-href="<?php echo $shop_now_url ?>" class="btn btn-lg btn--shop-now"><span>shop</span>now</a>
+            </div>
+          </div>
+        </section><!-- .col-xs-12 -->
