bb0f856fb3214493ba059f79e415e86b51127527
Bob Ware <bob.ware@purered.net>
2018-09-10 14:26:35 -0400

    Fix brand logos on SELP-4b

--- SELP-4b-tips/index.php
+++ SELP-4b-tips/index.php
@@ -4,6 +4,14 @@ require_once( $_SERVER['DOCUMENT_ROOT'] . '/inc/bs.php' );
 
 $slug = basename( dirname( __FILE__ ) );
 
+$prod_slug = '2018-09-05_Time-Saving-Dinner-Tips';
+set_prod_img_dir( $prod_slug );
+
+if ( !defined( 'SEARS_USE_SANDBOX_ASSETS' ) ) {
+  define( 'SEARS_USE_SANDBOX_ASSETS', true );
+  // define( 'SEARS_USE_SANDBOX_ASSETS', false );
+}
+
 
 $page_content = dirname( __FILE__ ) . '/page-content.php';
 
