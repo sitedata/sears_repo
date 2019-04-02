e6e169bd4dee35d5b65315251210c0822522d80c
Simone Anthony <santhony@PR1-DM-SimoneAnthony.local>
2019-04-02 09:10:51 -0400

    SELP-28 final Version

---
+++ SELP-28/page-content.php
@@ -0,0 +1,17 @@
+<?php
+/**
+Put the variables storing the copy, et al, into a separate file.
+*/
+require_once( dirname( __FILE__ ) . '/page-data.php' );
+
+/**
+IMPORTANT!
+HERO MUST BE OUTSIDE OF DIV.CONTAINER TO BE FULL WIDTH!
+*/
+?>
+<div class="cms-content cms-content-html padding-vert-xl SEARS--landing-page <?php echo $slug ?>" id="cms-content-1920-1920">
+
+<!-- SEE SELP-28-FinalVersion.HTML !!!!!! I built this page with Victor's HTML-->
+
+
+</div><!-- #cms-content-1920-1920.cms-content.cms-content-html -->
