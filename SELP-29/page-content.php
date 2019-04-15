6e48086c75aab148a8f1f138d9b220346e47410e
Simone Anthony <santhony@PR1-DM-SimoneAnthony.local>
2019-04-15 15:18:00 -0400

    SELP-29

---
+++ SELP-29/page-content.php
@@ -0,0 +1,186 @@
+<?php
+/**
+Put the variables storing the copy, et al, into a separate file.
+*/
+require_once( dirname( __FILE__ ) . '/page-data.php' );
+
+/**
+IMPORTANT!
+HERO MUST BE OUTSIDE OF DIV.CONTAINER TO BE FULL WIDTH!
+*/
+?>
+<div class="cms-content cms-content-html padding-vert-xl-top SEARS--landing-page <?php echo $slug ?>" id="cms-content-1920-1920">
+
+    <!-- BEGIN #hero -->
+    <section id="hero">
+        <div class="banner">
+            <img alt="Sears Hometown Store How to Take Care of Your Push Mower" src="http://sb6.hometown.netsuitestaging.com/assets/cms/purered/how-to-take-care-of-your-push-mower/01_masthead_HowToPushMower.jpg" class="img-responsive mqImage width100percent">
+        </div>
+    </section>
+    <!-- END #hero -->
+    <!-- extra wrapper around all these things that are spozta be 65px narrower than hero -->
+    <div class="">
+        <!-- HEADLINE -->
+        <section class="container-fluid headline width-adjust">
+            <!-- <div class="col-xs-12"> -->
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="headline--copy text-align-center font--primary2 font--700 ">
+                        The easiest way to get a great looking lawn is with a great lawn mower, so make sure you’re keeping your power lawn and garden equipment in tip-top shape by following these simple push mower maintenance tips and scheduling guide.</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 fake-bottom-border no-pad">
+                    <hr class="bordered--bottom bordered--sm hr-adjust first-line">
+                </div>
+            </div>
+            <!-- </div> -->
+        </section>
+        <section class="container-fluid width-adjust">
+            <div class="row">
+                <div class="col-xs-12 col-md-6 padding-horiz-md hidden-sm hidden-xs">
+                    <img src="http://sb6.hometown.netsuitestaging.com/assets/cms/purered/how-to-take-care-of-your-push-mower/02_Schedule.jpg" class="width100percent" alt="Man mowing lawn with a new Craftsman self-propelled gas mower ">
+                </div>
+                <div class="col-xs-12 col-md-6 padding-horiz-md">
+                    <h3 class="font--400 font--primary2 width100percent font-48">
+                        Why Buy a New Riding Mower?</h3>
+                    <p class="font--black font-18 lh-sm no-margin-bottom">
+                       <a href="/Lawn-Garden/Mowers-Tractors/Riding-Mowers-Tractors" data-link="/Lawn-Garden/Mowers-Tractors/Riding-Mowers-Tractors">Riding mowers</a> are the workhorse of lawn care. They can handle a wide variety of tasks including snow removal, tilling, aerating, fertilizing, seeding and hauling debris. If you have a large yard over a ½ acre or it takes you more than two hours to mow your lawn with a traditional push mower, then a riding mower is right for you and your landscape. </p>
+                       <p class="font--black font-18 lh-sm no-margin-bottom">
+                       When choosing a riding mower, consider your landscape. Do you have a flat yard or uneven terrain? Are there a lot of obstacles you’ll need to maneuver around like trees and flowerbeds? How large is your landscape? Zero-turn radius mowers and yard/lawn tractors are ideal for ½ acre or larger, but if you have a larger lot, a heavy-duty garden tractor would be a better fit. 
+                       </p><!-- <br> -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 fake-bottom-border no-pad">
+                    <hr class="bordered--bottom bordered--sm hr-adjust">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-6 padding-horiz-md four-pods">
+                    <img src="http://sb6.hometown.netsuitestaging.com/assets/cms/purered/how-to-take-care-of-your-push-mower/03_8hours.jpg" alt="Checking oil level on a push mower" class="img-responsive width100percent img-margin-bottom">
+                    <div class="padding-top-xl padding-horiz-md">
+                        <h3 class="font--400 font--primary2 width100percent font-48">
+                            Every 8 Hours </h3>
+                        <ul class="font--black font-18 lh-sm padding-horiz-md">
+                            <li><span>Check oil level – old or contaminated oil should be drained and replaced</span></li>
+                            <li><span>Inspect mower blades for wear or damage</span></li>
+                            <li><span>Clean the deck</span></li>
+                            <li><span>Brush off the mower to keep debris from collecting on the blade</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 padding-horiz-md four-pods">
+                    <img src="http://sb6.hometown.netsuitestaging.com/assets/cms/purered/how-to-take-care-of-your-push-mower/04_25hours.jpg" alt="Man replacing air filter on a push mower" class="img-responsive width100percent img-margin-bottom">
+                    <div class="padding-top-xl padding-horiz-md">
+                        <h3 class="font--400 font--primary2 width100percent font-48">
+                            Every 25 Hours </h3>
+                        <ul class="font--black font-18 lh-sm padding-horiz-md">
+                            <li><span>Check blade and engine mounting fasteners – tighten if loose</span></li>
+                            <li><span>Clean or replace air filter</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- div.row -->
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 fake-bottom-border no-pad">
+                    <hr class="bordered--bottom bordered--sm hr-adjust">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-6 padding-horiz-md four-pods">
+                    <img src="http://sb6.hometown.netsuitestaging.com/assets/cms/purered/how-to-take-care-of-your-push-mower/04_50hours.jpg" alt="cleaning oven using natural products" class="width100percent">
+                    <div class="padding-top-xl padding-horiz-md">
+                        <h3 class="font--400 font--primary2 width100percent font-48">
+                            Every 50 Hours </h3>
+                        <ul class="font--black font-18 lh-sm padding-horiz-md">
+                            <li><span>Change engine oil</span></li>
+                            <li><span>Lubricate the wheels</span></li>
+                            <li><span>Ensure the blade stops within 3 seconds of releasing the blade control bar</span></li>
+                            <li><span>Check for leaks in fuel system</span></li>
+                            <li><span>Replace or have blade sharpened</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 padding-horiz-md four-pods">
+                    <img src="http://sb6.hometown.netsuitestaging.com/assets/cms/purered/how-to-take-care-of-your-push-mower/05_Annually.jpg" alt="Grass being discharged from the side of a push mower" class="width100percent ">
+                    <div class="padding-top-xl padding-horiz-md">
+                        <h3 class="font--400 font--primary2 width100percent font-48">
+                            Annually</h3>
+                        <ul class="font--black font-18 lh-sm padding-horiz-md">
+                            <li><span>Replace air filter if not done at 25 hours</span></li>
+                            <li><span>Replace spark plug</span></li>
+                            <li><span>Clean cooling system</span></li>
+                            <li><span>Empty gas tank and drain oil at end of season </span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- div.row -->
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 fake-bottom-border no-pad">
+                    <hr class="bordered--bottom bordered--sm hr-adjust">
+                </div>
+            </div>
+    </div>
+    <!-- div.col-xs-12 -->
+    </section>
+    <!-- FOOTER-ISH HEADLINE -->
+    <section class="container headline footer-ish--headline">
+        <!-- <div class="fake-bottom-border">
+<hr class="visible-xs visible-sm bordered--bottom bordered--sm">
+</div> -->
+        <div class="col-xs-12">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p class="brand-logos__copy font--black lh-sm font--700 text-align-center brand-headline">
+                        Ready to shop mowers? At Sears Hometown Store, we have a large variety of mower models suitable for any sized yard: <a href="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&c=3721178&n=2&nav_search=1&search_return=all&Itemclass=Gas%20Self-Propelled%20Lawn%20Mowers" data-link="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&c=3721178&n=2&nav_search=1&search_return=all&Itemclass=Gas%20Self-Propelled%20Lawn%20Mowers" class="font--700">gas self-propelled</a>, <a href="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&Itemclass=Gas%20Push%20Mowers&c=3721178&n=2&nav_search=1&search_return=all" data-link="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&Itemclass=Gas%20Push%20Mowers&c=3721178&n=2&nav_search=1&search_return=all" class="font--700">gas push</a>, <a href="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&Itemclass=Zero%20Turn%20Tractors&c=3721178&n=2&nav_search=1&search_return=all" data-link="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&Itemclass=Zero%20Turn%20Tractors&c=3721178&n=2&nav_search=1&search_return=all" class="font--700">zero turn radius</a>, <a href="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&c=3721178&n=2&nav_search=1&search_return=all&Itemclass=Lawn%20Tractors" data-link="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&c=3721178&n=2&nav_search=1&search_return=all&Itemclass=Lawn%20Tractors" class="font--700">lawn tractors</a>, <a href="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&Itemclass=Yard%20Tractors&c=3721178&n=2&nav_search=1&search_return=all" data-link="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&Itemclass=Yard%20Tractors&c=3721178&n=2&nav_search=1&search_return=all" class="font--700">yard tractors</a> and <a href="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&c=3721178&n=2&nav_search=1&search_return=all&Itemclass=Garden%20Tractors" data-link="http://www.searshometownstores.com/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Riding%20Mowers%20%26%20Tractors&c=3721178&n=2&nav_search=1&search_return=all&Itemclass=Garden%20Tractors" class="font--700">garden tractors</a>. Need to fix your mower? We also carry <a href="/Lawn-Garden/Lawn-Garden-Accessories/Lawn-Mower-Parts-Accessories" data-link="/Lawn-Garden/Lawn-Garden-Accessories/Lawn-Mower-Parts-Accessories" class="font--700">lawn mower parts and accessories</a> and <a href="/Lawn-Garden/Lawn-Garden-Accessories/Tractor-Attachments-Accessories" data-link="/Lawn-Garden/Lawn-Garden-Accessories/Tractor-Attachments-Accessories" class="font--700">tractor parts and accessories</a> such as air and oil filters, mower blades, mower drive belts, and much more.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- BRAND LOGOS -->
+    <section class="container brand-logos padding-vert-md">
+        <!-- BRAND LOGOS HEADING -->
+        <div class="row">
+            <div class="col-xs-12 text-align-center">
+            </div>
+        </div>
+        <div class="row">
+            <div class="brand-logos--wrapper brand-logos-section">
+                <div class="brand-logo">
+                    <a href="/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&Itemclass=Cordless%20Rechargeable%20Mowers%5ECorded%20Electric%20Mowers%5EReel%20Mowers%5EGas%20Push%20Mowers%5EGas%20Self-Propelled%20Lawn%20Mowers&c=3721178&n=2&nav_search=1&search_return=all&Enginebrand=Craftsman">
+                        <img src="http://sb6.hometown.dev.kili.netsuitestaging.com/assets/cms/purered/buying-guide-lawn-mower/Craftsman.png" width="217" height="146" alt="Craftsman" class="">
+                    </a>
+                </div>
+                <div class="brand-logo">
+                    <a href="/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&Itemclass=Cordless%20Rechargeable%20Mowers%5ECorded%20Electric%20Mowers%5EReel%20Mowers%5EGas%20Push%20Mowers%5EGas%20Self-Propelled%20Lawn%20Mowers&c=3721178&n=2&nav_search=1&search_return=all&Brandname=Craftsman%20ProSeries">
+                        <img src="http://sb6.hometown.dev.kili.netsuitestaging.com/assets/cms/purered/buying-guide-lawn-mower/CraftsmanPro.png" width="240" height="146" alt="Craftsman Pro.png" class="">
+                    </a>
+                </div>
+                <div class="brand-logo">
+                    <a href="/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&Itemclass=Cordless%20Rechargeable%20Mowers%5ECorded%20Electric%20Mowers%5EReel%20Mowers%5EGas%20Push%20Mowers%5EGas%20Self-Propelled%20Lawn%20Mowers&c=3721178&n=2&nav_search=1&search_return=all&Brandname=Husqvarna">
+                        <img src="http://sb6.hometown.dev.kili.netsuitestaging.com/assets/cms/purered/buying-guide-lawn-mower/Husqvarna.png" width="240" height="146" alt="Husqvarna" class="">
+                    </a>
+                </div>
+                <div class="brand-logo">
+                    <a href="/s?Category0=Lawn%20%26%20Garden&Category1=Mowers%20%26%20Tractors&Category2=Push%20Mowers&Itemclass=Cordless%20Rechargeable%20Mowers%5ECorded%20Electric%20Mowers%5EReel%20Mowers%5EGas%20Push%20Mowers%5EGas%20Self-Propelled%20Lawn%20Mowers&c=3721178&n=2&nav_search=1&search_return=all&Brandname=Poulan%20Pro">
+                        <img src="http://sb6.hometown.dev.kili.netsuitestaging.com/assets/cms/purered/buying-guide-lawn-mower/Poulan.png" width="226" height="146" alt="Poulan" class="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 text-align-center">
+                <a href="/Lawn-Garden/Mowers-Tractors/Push-Mowers" data-href="/Lawn-Garden/Mowers-Tractors/Push-Mowers" class="btn btn-lg btn--shop-now red-cta"><span>Shop Push Mowers &#9658;</span></a>
+            </div>
+        </div>
+    </section><!-- .col-xs-12 -->
+</div>
+<!-- div.pading-horiz-xl -->
+</div>
+
+
+</div><!-- #cms-content-1920-1920.cms-content.cms-content-html -->
